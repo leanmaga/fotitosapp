587b5362824f08ea912237962dc1be43a29acb6d
Leandro Magallanes <lamagallanes@mobilecomputing.com.ar>
2024-10-14 00:03:21 -0300

    first deploy

--- src/app/page.js
+++ src/app/page.js
@@ -2,10 +2,16 @@ import { connectDB } from "@/libs/mongodb";
 import User from "@/models/user";
 
 async function loadUsers() {
-  await connectDB();
-  const users = await User.find();
-  return users;
+  try {
+    await connectDB();
+    const users = await User.find();
+    return users;
+  } catch (error) {
+    console.error("Error al cargar usuarios:", error);
+    return []; // Devuelve un arreglo vacío en caso de error
+  }
 }
+
 async function HomePage() {
   const users = await loadUsers();
   return (
--- src/libs/mongodb.js
+++ src/libs/mongodb.js
@@ -1,5 +1,14 @@
 import mongoose from "mongoose";
 
 export async function connectDB() {
-  await mongoose.connect(process.env.MONGODB_URL);
+  try {
+    await mongoose.connect(process.env.MONGODB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Conectado a MongoDB");
+  } catch (error) {
+    console.error("Error al conectar a MongoDB:", error);
+    throw new Error("No se pudo conectar a la base de datos");
+  }
 }

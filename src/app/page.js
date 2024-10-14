0e5ba50935de341e9c8b931e32ffd87a868cdb6d
Leandro Magallanes <lamagallanes@mobilecomputing.com.ar>
2024-10-14 00:10:11 -0300

    first deploy, modificar home

--- src/app/page.js
+++ src/app/page.js
@@ -1,25 +1,5 @@
-import { connectDB } from "@/libs/mongodb";
-import User from "@/models/user";
-
-async function loadUsers() {
-  try {
-    await connectDB();
-    const users = await User.find();
-    return users;
-  } catch (error) {
-    console.error("Error al cargar usuarios:", error);
-    return []; // Devuelve un arreglo vacío en caso de error
-  }
-}
-
-async function HomePage() {
-  const users = await loadUsers();
-  return (
-    <>
-      <h1>Users</h1>
-      <pre>{JSON.stringify(users, null, 2)}</pre>
-    </>
-  );
-}
+const HomePage = () => {
+  return <div>HOME PAGE</div>;
+};
 
 export default HomePage;
